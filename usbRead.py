ceb8b66d8fe79d6ff3df717bbd9677aaefdbefa1
sky <waimingsky2gmail.com>
2017-01-25 13:59:27 +0800

    change to class function

--- usbRead.py
+++ usbRead.py
@@ -2,72 +2,95 @@ import usb.core
 import usb.util
 import string 
 import struct
+import logging
+import logging.handlers
+from logging.handlers import RotatingFileHandler
+from logging import StreamHandler, Formatter
 
-def main():
-    device = usb.core.find(idVendor=0x0547, idProduct=0x1002)
+class usbRead():
+	def __init__(self):
+    		self._loggerInit()
+		self.logg = logging.getLogger('')
+		self.logg.info('PolyU Camera capture log file')
 
-    # use the first/default configuration
-    device.set_configuration()
+		self.device = usb.core.find(idVendor=0x0547, idProduct=0x1002)
 
-    # first endpoint
-    endpoint = device[0][(0,0)][0]
+		# use the first/default configuration
+		self.device.set_configuration()
 
-    # read a data packet
-    #data_packet = device.read(0x86,512,1000)
-    #for n in range(512):
-	#byte_data = data_packet[n]
-        #bit_string =  int2base(byte_data,2)
-        #print bit_string
+		# first endpoint
+		self.endpoint = self.device[0][(0,0)][0]
+		
+		self.readBitString()
         
+	def readBitString(self): 
+		while True:
+			try:
+				data_packet = self.device.read(0x86,512,1000)
+				for n in range(512):
+					byte_data = data_packet[n]
+					bit_string =  self.int2base(byte_data,2)
+					print bit_string
+	
+					#looking for new frame
+					if bit_string[0] == '1' and bit_string[1] == '0' and bit_string[2] == '1' and bit_string[3] == '0':
+						print "invalid frame******************"
+						self.logg.info("invalid frame **************")
+						if bit_string[0] == '1' and bit_string[1] == '0' and bit_string[2] == '1' and bit_string[3] == '1':
+							print "new frame started #################################"
+							self.logg.info("new frame started ##########################")
+						else:
+							print "waiting for new frame to start *****************"
+							self.logg.info("waiting for new frame to start *****************")
+                   
 
-    while True:
-        try:
-            data_packet = device.read(0x86,512,1000)
-            for n in range(512):
-        	byte_data = data_packet[n]
-        	bit_string =  int2base(byte_data,2)
-        	print bit_string
-		if bit_string[0] == '1' and bit_string[1] == '0' and bit_string[2] == '1' and bit_string[3] == '0':
-			print "invalid frame****************************************************"
-		if bit_string == '00000000':
-			print "null value ******************************************************"
-
-        except usb.core.USBError as e:
-            data = None
-            if e.args == ('Operation timed out',):
-
-                continue
+			except usb.core.USBError as e:
+				data_packet = None
+				self.logg.error("USB ERROR. %s"%(e.message))
+				if e.args == ('Operation timed out',):	
+					continue
 
 
-def int2base(x, base):
-    digs = string.digits + string.letters
+	def int2base(self,x, base):
+		digs = string.digits + string.letters
 
-    if x < 0:
-        sign = -1
-    elif x == 0:
-        #return digs[0]
-	return '00000000'
-    else:
-        sign = 1
+		if x < 0:
+			sign = -1
+		elif x == 0:
+			#return digs[0]
+			return '00000000'
+		else:
+			sign = 1
 
-    x *= sign
-    digits = []
+		x *= sign
+		digits = []
 
-    while x:
-        digits.append(digs[x % base])
-        x /= base
+		while x:
+			digits.append(digs[x % base])
+			x /= base
 
-    if sign < 0:
-        digits.append('-')
+		if sign < 0:
+			digits.append('-')
 
-    #digits.reverse()
-    #print len(digits)
-    add_zero_no = 8 - len(digits)
-    for n in range (add_zero_no):
-	digits.append(digs[0 % base])
+		#digits.reverse()
+		#print len(digits)
+		add_zero_no = 8 - len(digits)
+		for n in range (add_zero_no):
+			digits.append(digs[0 % base])
 
-    return ''.join(digits)
+		return ''.join(digits)
 
+	def _loggerInit(self,file_name = 'debugLog.log'):
+		folder = 'log'
+		try:
+			os.makedirs(folder)
+		except Exception as e:
+			pass   # pass if log folder exists
+		logg = logging.getLogger('')
+		logg.setLevel(logging.DEBUG)
+		handler = RotatingFileHandler(folder +'/'+ file_name, backupCount=200, maxBytes=1024*500)
+		handler.setFormatter(Formatter('%(asctime)s %(filename)s[line:%(lineno)d] [%(levelname)s] %(message)s'))
+		logg.addHandler(handler)
 
 if __name__ == '__main__':
-  main()
+	read = usbRead()
